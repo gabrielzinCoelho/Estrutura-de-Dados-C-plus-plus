ff0163ed848d768a99e6381db1c8d8012d52bc1a
Gabriel Coelho Costa <coelhocostag@gmail.com>
2023-12-23 16:35:51 -0300

    Implementacao e README:  filas arr

--- fila/filaArr/README.md
+++ fila/filaArr/README.md
@@ -3,3 +3,13 @@
 Confira a implementação de uma [fila utilizando arrays](filaArr.cpp) em C++.
 
 ## Detalhes da implementação
+
+Assim como nas pilhas baseadas em arrays, a implementação de filas em arrays possui como vantagem a **maior velocidade nas operações de empilhamento e desempilhamento**.
+
+De forma similar, essa abordagem também é acompanhada pela **limitação na capacidade da estrutura**, que permanece fixa até que o array seja **redimensionado** (processo computacionalmente caro). Além disso, na grande parte das vezes o **espaço alocado é maior do que o necessário**.
+
+Para que as operações ocorram de maneira adequada, a fila armazena os **indíces do array correspondentes ao início e fim** da estrutura. Ao **enfileirar** um elemento, o **fim avança uma posição**, e, ao **desenfileirar** um elemento, o **início avança uma posição**. 
+
+Um ponto importante a ser observado é que implementações de filas em arrays utilizam do conceito de **filas circulares**. Dessa forma, após sucessivas remoções e inserções, é possível que o indíce de fim se encontre em uma posição anterior ao indíce de início. Isso é obtido da seguinte forma: toda vez que algum dos indíces (início ou fim) avance uma posição, basta **calcular o mod do novo valor**.
+
+Além disso, vale ressaltar a importância do atributo **tamanho** nesse contexto: não é possível calcular (sem ter conhecimento da última operação feita sobre a estrutura) o número de elementos da fila utilizando apenas os indíces de início e fim.
--- fila/filaArr/filaArr.cpp
+++ fila/filaArr/filaArr.cpp
@@ -1,6 +1,137 @@
 #include <iostream>
+#include <stdexcept>
+
+typedef int Dado;
+const int INDEX_NULO{-1};
+
+class FilaArr{
+    private:
+        int indexInicio, indexFim;
+        unsigned int tamanho;
+        const unsigned int capacidade;
+        Dado *dadoArr;
+    public:
+        FilaArr(int capacidade = 10);
+        ~FilaArr();
+        void enfileira(const Dado &d);
+        Dado desenfileira();
+        Dado espiarInicio();
+        void limparTudo();
+        unsigned int getTamanho();
+        bool vazia();
+        bool cheia();
+
+};
+
+FilaArr::FilaArr(int capacidade) : capacidade(capacidade) {
+    indexInicio = indexFim = INDEX_NULO;
+    dadoArr = new Dado[capacidade];
+    tamanho = 0;
+}
+
+FilaArr::~FilaArr(){
+    delete[] dadoArr;    
+}
+
+void FilaArr::enfileira(const Dado &d){
+
+    if(cheia())
+        throw std::runtime_error("Erro: fila cheia!");
+    
+    if(vazia())
+        indexInicio = 0;
+    indexFim = (indexFim + 1) % capacidade;
+
+    dadoArr[indexFim] = d;
+
+    tamanho++;
+
+}
+
+Dado FilaArr::desenfileira(){
+    if(vazia())
+        throw std::runtime_error("Erro: fila vazia!");
+
+    Dado temp = dadoArr[indexInicio];
+    tamanho--;
+
+    if(vazia())
+        indexInicio = indexFim = INDEX_NULO;
+    else
+        indexInicio = (indexInicio + 1) % capacidade;
+
+    return temp;
+    
+}
+
+Dado FilaArr::espiarInicio(){
+    if(vazia())
+        throw std::runtime_error("Erro: fila vazia!");
+
+    return dadoArr[indexInicio];
+    
+}
+
+void FilaArr::limparTudo(){
+    indexInicio = indexFim = INDEX_NULO;
+    tamanho = 0;
+}
+
+unsigned int FilaArr::getTamanho(){
+    return tamanho;
+}
+
+bool FilaArr::vazia(){
+    return !bool(tamanho);
+}
+
+bool FilaArr::cheia(){
+    return (tamanho == capacidade);
+}
 
 int main(){
+
+    FilaArr fila;
+    Dado d;
+    char opcao;
+
+    do{
+
+        try{
+
+            std::cin >> opcao;
+
+            switch(opcao){
+                case 'i': // enfileirar elemento
+                    std::cin >> d;
+                    fila.enfileira(d);
+                    break;
+                case 'r': // desenfileirar elemento
+                    d = fila.desenfileira();
+                    std::cout << d << "\n";
+                    break;
+                case 'l': // limpar tudo
+                    fila.limparTudo();
+                    break;
+                case 'e':
+                    d = fila.espiarInicio();
+                    std::cout << d << "\n";
+                    break;
+                case 't':
+                    std::cout << "tamanho: " << fila.getTamanho() << "\n";
+                    break;
+                case 'x':
+                    break;
+                default:
+                    std::cout << "opcao invalida\n";
+            }
+
+
+        }catch(std::runtime_error &err){
+            std::cout << err.what() << "\n";
+        }
+
+    }while(opcao != 'x');
 
     return 0;
 }
