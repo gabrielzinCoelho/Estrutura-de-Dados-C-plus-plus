19fc4578f59a29b9400616504c92dade745c8858
Gabriel Coelho Costa <coelhocostag@gmail.com>
2024-04-11 14:44:40 -0300

    att implementacao listaEncadeada

--- lista/listaEncadeada/listaEncadeada.cpp
+++ lista/listaEncadeada/listaEncadeada.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 typedef int Dado;
 
@@ -15,10 +16,110 @@ class Lista{
     private:
         Noh *inicio, *fim;
         int tamanho;
+        void inserirListaVazia(const Dado &d);
+        Noh* iteradorBuscaValor(const Dado &d);
+        Noh* iteradorBuscaPosicao(int pos);
     public:
         Lista();
         ~Lista();
+        void limparLista();
+        bool vazia();
+        Dado removerInicio();
+        Dado removerFim();
+        Dado removerValor();
+        Dado removerPosicao();
+        void inserirInicio(const Dado &d);
+        void inserirFim(const Dado &d);
+        void inserirPosicao(const Dado &d, int pos);
+        Dado buscaValor();
+        Dado buscaPosicao();
 };
+
+Lista::Lista() : inicio(nullptr), fim(nullptr), tamanho(0) {}
+
+Lista::~Lista(){
+    limparLista();
+}
+
+void Lista::limparLista(){
+    while(!vazia())
+        removerInicio();
+}
+
+bool Lista::vazia(){
+    return !tamanho;
+}
+
+Noh* Lista::iteradorBuscaPosicao(int pos){
+    if(pos < 0 || pos > (tamanho - 1))
+        throw std::runtime_error("Erro: posição de busca invalida.\n");
+    
+    int indexBusca{0};
+    Noh* aux{inicio};
+
+    while(indexBusca <= pos){
+        indexBusca++;
+        aux = aux->proximo;
+    }
+
+    return aux;
+}
+
+Noh* Lista::iteradorBuscaValor(const Dado &d){
+
+    Noh* aux{inicio}, *anterior{nullptr};
+
+    while(aux != nullptr){
+        if(aux){}
+    }
+
+    return aux;
+}
+
+void Lista::inserirListaVazia(const Dado& d){
+
+    Noh *novo = new Noh(d);
+    inicio = fim = novo;
+    tamanho = 1;
+
+}
+
+void Lista::inserirInicio(const Dado& d){
+    if(vazia())
+        inserirListaVazia(d);
+    
+    Noh *novo = new Noh(d);
+    novo->proximo = inicio;
+    inicio = novo;
+    tamanho++;
+}
+
+void Lista::inserirFim(const Dado& d){
+    if(vazia())
+        inserirListaVazia(d);
+    
+    Noh *novo = new Noh(d);
+    fim->proximo = novo;
+    fim = novo;
+    tamanho++;
+}
+
+void Lista::inserirPosicao(const Dado& d, int pos){
+    
+    if(pos < 0 || pos > tamanho)
+        throw std::runtime_error("Erro: posição de insercao invalida.\n");
+    
+    if(pos == 0)
+        inserirInicio(d);
+    else if(pos == tamanho)
+        inserirFim(d);
+    else{
+        Noh *novo = new Noh(d);
+        Noh* anteriorPos = iteradorBuscaPosicao(pos - 1);
+        novo->proximo = anteriorPos->proximo;
+        anteriorPos->proximo = novo;
+    }
+}
 
 int main(){
 
