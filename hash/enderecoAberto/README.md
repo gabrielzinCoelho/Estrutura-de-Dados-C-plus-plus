9bf1ce01e2a866375094e9e8438a064bd120ca33
Gabriel Coelho Costa <coelhocostag@gmail.com>
2024-04-22 17:37:34 -0300

    add implementacao hashEA

--- hash/enderecoAberto/README.md
+++ hash/enderecoAberto/README.md
@@ -3,3 +3,19 @@
 Confira a implementação de uma **[tabela hash com endereçamento aberto](hashEncadeamento.cpp)** em C++.
 
 ## Detalhes da implementação
+
+No tratamento de colisão por Endereçamento Aberto, sempre que há a tentativa de inserção em uma posição já ocupada, procura-se a próxima posição disponível para armazenar o novo elemento.
+
+Essa estratégia exige alterações na maneira de fazer a busca e a remoção de um determinado elemento.
+
+Durante a busca, ao mapear uma determinada chave para uma posição e o elemento lá armazenado não ser o elemento que se procura, é necessário percorrer todas as posições subsequentes (array circular) até que se encontre o elemento, ou uma posição vazia ou, então, até que se retorne a posição inicial da busca. Isso ocorre devido ao fato de, ao não inserir um dado elemento em sua posição correta, não há como garantir se ele se encontra na tabela ou não até que as posições subsequentes sejam verificadas.
+
+O mesmo cuidado deve ser aplicado também às remoções. Nesse caso, ao remover um elemento, não se pode simplesmente tornar a posição anteriormente ocupada por ele vazia. Isso comprometeria a lógica utilizada por uma eventual busca de um elemento qualquer posterior a  essa remoção. Sendo assim, duas alternativas podem ser adotadas:
+
+1. Deslocamento dos dados para a posição desocupada.
+
+O deslocamento dos dados para a posição desocupada exige a verificação de, enquanto não se retorna à posição inicial ou chega-se em uma posição vazia, se o elemento não deve ser reposicionado. Caso determinado elemento se encontre em uma posição correspondente ao cálculo de sua função hash, então ele não deve ser reposicionado. 
+
+1. Marcar a posição livre como disponível para uso, utilizando um valor simbólico, como -1, para tal. Assim, podemos diferenciar uma posição que sempre esteve livre de uma que se tornou após a remoção de um elemento que a ocupava.
+
+Após um grande número de remoções, essa estratégia começa a comprometer a eficiência da busca, sendo necessário realizar a reinserção dos elementos atuais.
--- hash/enderecoAberto/hashEnderecoAberto.cpp
+++ hash/enderecoAberto/hashEnderecoAberto.cpp
@@ -1,4 +1,123 @@
 #include <iostream>
+#include <stdexcept>
+
+typedef int Dado;
+
+struct Elemento{
+    int chave;
+    Dado dado;
+};
+
+const Elemento INVALIDO = {-1, -1};
+const Elemento REMOVIDO = {-2, -2};
+
+bool operator==(const Elemento &elementoA, const Elemento &elementoB){
+    return elementoA.chave == elementoB.chave;
+}
+
+bool operator!=(const Elemento &elementoA, const Elemento &elementoB){
+    return elementoA.chave != elementoB.chave;
+}
+
+class HashEA{
+    private:
+        Elemento *tabela;
+        int capacidade, tamanho;
+        int funcaoHash(int chave);
+        bool verificarOcorrencia(int chave);
+    public:
+        HashEA(int capacidade = 19);
+        ~HashEA();
+        void inserir(Elemento e);
+        Dado remover(int chave);
+        Dado buscar(int chave);
+        void imprimir();
+};
+
+HashEA::HashEA(int capacidade){
+    this->capacidade = capacidade;
+    tamanho = 0;
+    tabela = new Elemento[capacidade];
+
+    for(int i{0}; i<capacidade; i++)
+        tabela[i] = INVALIDO;
+}
+
+HashEA::~HashEA(){
+    delete[] tabela;
+}
+
+int HashEA::funcaoHash(int chave){
+    return chave % capacidade;
+}
+
+bool HashEA::verificarOcorrencia(int chave){
+
+    int posFinal = funcaoHash(chave);
+    int pos = posFinal;
+
+    do{
+
+        if(tabela[pos].chave == chave)
+            return true;
+        if(tabela[pos] == INVALIDO)
+            return false;
+
+        pos = (pos + 1) % capacidade;
+
+    }while(pos != posFinal);
+      
+    return false;
+}
+
+void HashEA::inserir(Elemento e){
+    
+    if(tamanho == capacidade)
+        throw std::runtime_error("Tabela Hash cheia.");
+    
+    if(e.chave < 0)
+        throw std::runtime_error("Valor de chave invalida para insercao.");
+    
+    if(verificarOcorrencia(e.chave))
+        throw std::runtime_error("Chave " + std::to_string(e.chave) + " já existente.");
+    
+    int pos = funcaoHash(e.chave);
+
+    while(tabela[pos] != INVALIDO && tabela[pos] != REMOVIDO)
+        pos = (pos + 1) % capacidade;
+    
+    tabela[pos] = e;
+    tamanho++;
+
+}
+Dado HashEA::remover(int chave){
+
+}
+Dado HashEA::buscar(int chave){
+
+    int posFinal = funcaoHash(chave);
+    int pos = posFinal;
+
+    do{
+
+        if(tabela[pos].chave == chave)
+            return tabela[pos].dado;
+        if(tabela[pos] == INVALIDO)
+            throw std::runtime_error("Chave " + std::to_string(chave) + " inexistente.");
+
+        pos = (pos + 1) % capacidade;
+
+    }while(pos != posFinal);
+      
+    throw std::runtime_error("Chave " + std::to_string(chave) + " inexistente.");
+
+}
+void HashEA::imprimir(){
+    for(int i{0}; i<capacidade; i++)
+        std::cout << tabela[i].chave << (i == capacidade - 1 ? "" : " - "); 
+    std::cout << "\n";
+}
+
 
 int main(){
 
